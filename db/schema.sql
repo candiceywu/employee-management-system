0281646073518c06ae23ce2b3f02e32d47ed4f7c
candiceywu <candice.ying.wu@gmail.com>
2021-09-17 09:52:12 -0700

    edited index.js

--- db/schema.sql
+++ db/schema.sql
@@ -1,26 +1,38 @@
+--- clear database ---
 DROP DATABASE IF EXISTS company_db;
+
+--- create database ---
 CREATE DATABASE company_db;
 
+--- activate database ---
 USE company_db;
 
+--- create departments database ---
 CREATE TABLE departments (
   name VARCHAR(30) NOT NULL,
-  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY
+  id INT AUTO_INCREMENT NOT NULL,
+  PRIMARY KEY (id)
 );
 
+--- create roles database ---
 CREATE TABLE roles (
   title VARCHAR(30) NOT NULL,
-  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  id INT AUTO_INCREMENT NOT NULL,
   department_id INT NOT NULL,
-  salary DECIMAL NOT NULL
+  salary DECIMAL NOT NULL,
+  PRIMARY KEY (id)
+  FOREIGN KEY (department_id)
+  REFERENCES departments(id)
 );
 
+--- create empoloyees database ---
 CREATE TABLE employees (
-  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  id INT AUTO_INCREMENT NOT NULL,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  job_title VARCHAR(30) NOT NULL,
+  roles_id INT NOT NULL,
   department VARCHAR(30) NOT NULL,
-  salary INT NOT NULL,
-  manager INT NOT NULL
-);
+  salary DECIMAL NOT NULL,
+  manager_id INT NOT NULL,
+  PRIMARY KEY (id)
+ 
--- index.js
+++ index.js
@@ -2,3 +2,27 @@ const cTable = require('console.table');
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const db = require('/db');
+
+
+
+//function for inquirer questions
+const init = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'mainMenu',
+                message: 'What would you like to do?',
+                choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role']
+            },
+
+
+
+
+
+
+        ])
+}
+
+//call the function to execute the question prompts
+init();
